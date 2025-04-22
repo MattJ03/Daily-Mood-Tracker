2fb4d3c095fb4303d92e5a4e93688c64c7ff0541
Matt Shaw <Shaw-M18@ulster.ac.uk>
2025-04-22 20:56:31 +0100

    new frag for enter mood

---
+++ app/src/main/java/com/example/dailymoodtracker/EnterMoodFragment.java
@@ -0,0 +1,18 @@
+package com.example.dailymoodtracker;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.content.Intent;
+import android.widget.TextView;
+import android.view.View;
+import android.widget.Toast;
+import androidx.fragment.app.Fragment;
+
+public class EnterMoodFragment extends Fragment{
+
+    public EnterMoodFragment() {
+        //Empty constructor which is needed for fragments
+    }
+
+    
+}
