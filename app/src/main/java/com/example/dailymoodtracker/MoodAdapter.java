887e5ca1613f3fd3dddfb7823dedd330f06e2978
Matt Shaw <Shaw-M18@ulster.ac.uk>
2025-05-01 00:04:30 +0100

    adapter for onBindViewModl and onCreateViewHolder

---
+++ app/src/main/java/com/example/dailymoodtracker/MoodAdapter.java
@@ -0,0 +1,44 @@
+package com.example.dailymoodtracker;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.LiveData;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class MoodAdapter extends RecyclerView.Adapter<MoodAdapter.MoodViewHolder> {
+
+    private List<MoodEntry> moodList;
+
+    public MoodAdapter(List<MoodEntry> moodList) {
+        this.moodList = moodList;
+    }
+
+    public void setMoodList(List<MoodEntry> newList) {
+        this.moodList = newList;
+        notifyDataSetChanged();
+    }
+
+    @NonNull
+    @Override
+    public MoodViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_mood, parent, false);
+        return new MoodViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewModel(@NonNull MoodViewHolder holder, int position) {
+        MoodEntry entry = moodList.get(position);
+        holder.moodText.setText(entry.mood);
+        holder.moodNote.setText(entry.note);
+    }
+
+    @Override
+
+}
