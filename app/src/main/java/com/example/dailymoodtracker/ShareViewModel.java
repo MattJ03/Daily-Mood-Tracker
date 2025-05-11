e5712f2a23d3f658d863f3ba920dc1c190be72f5
Matt Shaw <Shaw-M18@ulster.ac.uk>
2025-05-11 21:28:13 +0100

    getters and setters for the sharedviewmodel

--- app/src/main/java/com/example/dailymoodtracker/ShareViewModel.java
+++ app/src/main/java/com/example/dailymoodtracker/ShareViewModel.java
@@ -1,4 +1,17 @@
 package com.example.dailymoodtracker;
 
-public class ShareViewModel {
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class ShareViewModel extends ViewModel {
+
+   private final MutableLiveData<String> photoPath = new MutableLiveData<>();
+
+   public void setPhotoPath(String path) {
+       photoPath.setValue(path);
+   }
+
+   public MutableLiveData<String> getPhotoPath() {
+       return getPhotoPath();
+   }
 }
