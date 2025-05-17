82fe552ea8ed7187e18ed30a5c0fb6f55745b9d6
Matt Shaw <Shaw-M18@ulster.ac.uk>
2025-05-17 18:21:36 +0100

    added DAO methods for get all moods for each hsppy, sad and neutral

--- app/src/main/java/com/example/dailymoodtracker/EnterMoodDAO.java
+++ app/src/main/java/com/example/dailymoodtracker/EnterMoodDAO.java
@@ -16,8 +16,17 @@ public interface EnterMoodDAO {
     void insert(MoodEntry entry);
 
     @Query("SELECT * from MOOD_ENTRIES")
-    List<MoodEntry> getAllMoodsSync();
+    LiveData<List<MoodEntry>> getAllMoodsSync();
 
     @Query("SELECT * FROM mood_entries ORDER BY id DESC")
     LiveData<List<MoodEntry>> getAllMoods();  // Using LiveData to observe the changes
+
+    @Query("SELECT * FROM mood_entries WHERE mood = 'happy'")
+    LiveData<List<MoodEntry>> getAllHappyMoods();
+
+    @Query("SELECT * FROM mood_entries WHERE mood = 'neutral'")
+    LiveData<List<MoodEntry>> getAllNeutralMoods();
+
+    @Query("SELECT * FROM mood_entries WHERE mood = 'sad'")
+    LiveData<List<MoodEntry>> getAllSadMoods();
 }
--- app/src/main/java/com/example/dailymoodtracker/StatisticsFragment.java
+++ app/src/main/java/com/example/dailymoodtracker/StatisticsFragment.java
@@ -8,6 +8,7 @@ import android.widget.EditText;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 
 public class StatisticsFragment extends Fragment {
 
@@ -15,7 +16,11 @@ public StatisticsFragment() {
 
 }
 
-
+EditText numberHappy, numberSad, numberNeutral;
+MoodViewModel moodViewModel;
+int[] happyCount;
+int[] neutralCount;
+int[] sadCount;
 
 @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -24,6 +29,16 @@ public StatisticsFragment() {
 
 public void onViewCreated(@NonNull View view, @NonNull Bundle savedInstanceState) {
     super.onViewCreated(view, savedInstanceState);
+
+    numberHappy = view.findViewById(R.id.editTextNumberHappy);
+    numberNeutral = view.findViewById(R.id.editTextNumberNeutral);
+    numberSad = view.findViewById(R.id.editTextNumberSad);
+
+    moodViewModel = new ViewModelProvider(this).get(MoodViewModel.class);
+
+    moodViewModel.getAllMoods();
+
+
 
 
 }
